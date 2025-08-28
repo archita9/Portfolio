edf44b9ef0e8402ccb4271277745760165ca8ba8
archita9 <architagoyal7@gmail.com>
2025-08-28 14:09:17 +0530

    Initial acaommit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Archita Goyal </span>
+            from <span className="purple"> Panchkula, India.</span>
+            <br />
+            I am currently pursuing bachelor's of technology from CGC, Landran.
+            <br />
+            in Artificial Intelligence & Data Science
+            
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Reading
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing  Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Powerpoint slides
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Archita Goyal</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "UI/UX Developer",
+          "Software Developer",
+          "MERN Stack Developer",
+          "AI Developer",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
